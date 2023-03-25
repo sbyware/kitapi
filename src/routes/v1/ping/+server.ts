aee37970593c2c959327b72b986a1426680ebb97
sby051 <91495085+sby051@users.noreply.github.com>
2023-03-25 13:32:55 +0000

    added helper HTTPCodes object

--- src/routes/v1/ping/+server.ts
+++ src/routes/v1/ping/+server.ts
@@ -1,5 +1,6 @@
 import { type RequestHandler, json } from "@sveltejs/kit";
+import { HTTPCodes } from "@constants";
 
 export const GET = (async () => {
-    return json({ hello: "world" }, { status: 200 });
+    return json({ hello: "world" }, { status: HTTPCodes.NotImplemented });
 }) satisfies RequestHandler;
